cc158cb6a53b7bd6a935e7549d9d3778cd6c8772
jassmanni <jassmanniq@gmail.com>
2020-02-06 15:26:21 -0400

    login basico

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'register', to: 'users#new'
+  post 'register', to: 'users#create'
+  get 'home', to: 'home#index'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
