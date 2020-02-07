54df9c263c7fafcf5ff222e6b25f8783a27b1f1d
jassmanni <jassmanniq@gmail.com>
2020-02-07 12:16:16 -0400

    show html listo para comentarios

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,11 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
