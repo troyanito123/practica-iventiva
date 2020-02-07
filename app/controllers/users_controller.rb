5e99bff0bae951e4b21913fb8a81c54334d9d700
jassmanni <jassmanniq@gmail.com>
2020-02-07 17:57:33 -0400

    erroes de acceso en editar usuario corregido

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
 
   skip_before_action :logged_in_user, only: [:new, :create]
-
   before_action :user_invited, only: [:new, :create]
+  before_action :set_user, only: [:edit, :update]
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
 
   def new
     @user = User.new
@@ -19,12 +20,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       redirect_to home_path
     else
@@ -36,4 +34,20 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  private
+  def handle_record_not_found
+    flash[:danger] = I18n.t 'not_current_user'
+    redirect_to home_path
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      flash[:danger] = I18n.t 'not_current_user'
+      redirect_to home_path
+    end
+  end
+
 end
