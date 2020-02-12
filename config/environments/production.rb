d25924b85d8c5217199004b8dc8acd0aee83e61c
jassmanni <jassmanniq@gmail.com>
2020-02-12 11:45:51 -0400

    Configuracion de sengrid para enviar email y vista del email cambiada

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,15 +110,16 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {host: "https://foro-practica.herokuapp.com"}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings ={
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: "example.com",
-      authentication: "plain",
-      # enable_starttls_auto: true,
-      user_name: ENV['GOOGLE_USERNAME'],
-      password: ENV['GOOGLE_PASSWORD']
+  host = 'foro-practica.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
   }
 end
